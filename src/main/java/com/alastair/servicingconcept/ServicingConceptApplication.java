9433e6ccb2e1e76d9a758476cbecf9715a511af7
Alastair <Alastair>
2017-06-02 22:17:40 +0100

    initial commit

---
+++ src/main/java/com/alastair/servicingconcept/ServicingConceptApplication.java
@@ -0,0 +1,12 @@
+package com.alastair.servicingconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicingConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicingConceptApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alastair/servicingconcept/ServicingConceptApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alastair.servicingconcept;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicingConceptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
